ecf48d647bf73d8a10dbeba3ad3246452d7eaced
gilanggriawan <gilanganggriawan19@gmail.com>
2022-05-19 09:58:04 +0700

    menambahkan vendor prefix

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Popovers
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+const popoverList = [...popoverTriggerList].map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl));
+
+const popover = new bootstrap.Popover(".popover-dismiss", {
+  trigger: "focus",
+});
