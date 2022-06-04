9924fc117d24f797b0f9492d292b23b4376ca52e
gilanggriawan <gilanganggriawan19@gmail.com>
2022-06-04 15:40:33 +0700

    menambahkan konten portfolio

--- js/script.js
+++ js/script.js
@@ -6,3 +6,27 @@ const popoverList = [...popoverTriggerList].map((popoverTriggerEl) => new bootst
 const popover = new bootstrap.Popover(".popover-dismiss", {
   trigger: "focus",
 });
+
+// Scroll
+
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop();
+
+  // Portfolio
+  if (wScroll > $(".portfolio").offset().top - 250) {
+    $('.portfolio').each(function (i)) {
+      setTimeout(function (s) {
+        $('.portfolio .portfolio-container').eq(i).addClass('fall');
+      }, 300 * (i + 1));
+    }
+  }
+});
+
+// // Portfolio
+// if (wScroll > $(".hobby").offset().top - 250) {
+//   $(".hobby .thumbnail").each(function (i) {
+//     setTimeout(function (s) {
+//       $(".hobby .thumbnail").eq(i).addClass("muncul");
+//     }, 300 * (i + 1));
+//   });
+// }
